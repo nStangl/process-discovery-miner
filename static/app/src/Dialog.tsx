67b8bbf26955a5c7f74e0e58b4fed22c5516732a
Niklas Stangl <stangl@protonmail.com>
2022-01-27 15:23:52 +0100

    Make Dialog less ugly

--- static/app/src/Dialog.tsx
+++ static/app/src/Dialog.tsx
@@ -9,6 +9,7 @@ import CloseIcon from "@mui/icons-material/Close";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
 
 export { InfoDialog, AboutDialog };
 export interface DialogTitleProps {
@@ -80,10 +81,13 @@ function InfoDialog() {
         >
           How to use
         </BootstrapDialogTitle>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            To use the alpha miner upload a log file either as a .xml or .xes
-            file. See{" "}
+        <DialogContent dividers sx={{ p: 2 }}>
+          <Box sx={{ fontWeight: "medium", fontSize: 16, m: 1 }}>
+            1. Select the process discovery algorithm.{" "}
+          </Box>
+
+          <Box sx={{ fontWeight: "medium", fontSize: 16, m: 1 }}>
+            2. Upload a log file as a .xml or .xes file. See{" "}
             <Link
               href="https://lehre.bpm.in.tum.de/~pm-prak/datasets/"
               target="_blank"
@@ -91,14 +95,20 @@ function InfoDialog() {
               here
             </Link>{" "}
             for examples on how the log is supposed to be structured.
-            <br />
+          </Box>
+
+          <Box sx={{ fontWeight: "medium", fontSize: 16, m: 1 }}>
+            3. The alpha miner then processes the log and returns a petri net.
+          </Box>
+
+          <Typography gutterBottom>
+            The graph is interactive and can be saved as a JSON or PNG. Note
+            that it also saves the layout.
           </Typography>
           <Divider />
           <Typography gutterBottom>
-            The alpha miner then processes the log and returns a petri net.
-            <br />
-            The graph is interactive and can be saved as a JSON or PNG. Note
-            that it also saves the layout.
+            The backend currently supports files with a maximum size up to 50 MB.
+            This could easily be changed in the settings.
           </Typography>
         </DialogContent>
       </BootstrapDialog>
@@ -138,21 +148,45 @@ function AboutDialog() {
         </BootstrapDialogTitle>
         <DialogContent dividers>
           <Typography gutterBottom>
-            This is an implementation of a easy to use alpha miner. The
-            implementation is based on [1].
+            This is an implementation of easy to use process discovery
+            algorithms. It currently supports the Alpha Miner and the Alpha+
+            Miner.
             <br />
+            If you are unsure which mining alogrithm to choose, take the Alpha+
+            Miner.
+            <br />
+            The implementation of the Alpha Miner is based on [1].
+            <br />
+            The implementation of the Alpha+ Miner is mainly based on [2].
+            <br />
+            This project was made for my Bachelor internship 'Implementation of
+            Process Mining Algorithms: Transformative Business Knowledge'.
+            <br />
+            The project is made with React.js, Material UI and Typescript. As
+            well as Haskell and Yesod for the webserver.
           </Typography>
           <Divider />
           <Typography gutterBottom>
+            <Box sx={{ fontWeight: "medium", fontSize: 16, m: 1 }}>
+              Mentionable Sources{" "}
+            </Box>
             [1]: Van Der Aalst, Wil. Process Mining, Data Science in Action
             [2016]
             <br />
-            Favicon source:{" "}
+            [2]: ML Wiki.{" "}
+            <Link
+              href="https://http://mlwiki.org/index.php/Alpha_Algorithm#The_Alpha_Plus_Algorithm"
+              target="_blank"
+            >
+              The Alpha Plus Algorithm
+            </Link>
+            <br />
+            Favicon:{" "}
             <Link
               href="https://www.flaticon.com/free-icon/pickaxe_663361"
               target="_blank"
             >
-              here
+              source
             </Link>{" "}
           </Typography>
         </DialogContent>
